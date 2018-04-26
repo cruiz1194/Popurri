1c75617235e1d6675da3dac4bf84ebcd75f43658
cruiz1194 <vakamacarlos19_94@outlook.com>
2018-04-25 21:14:55 -0500

    Added Swift stuff and changed gitignore. Added MacOS rules to gitignore

---
+++ Swifty_Stuff/Swifty_Stuff/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swifty_Stuff
+//
+//  Created by Carlos ruiz on 25/04/18.
+//  Copyright © 2018 Carlos ruiz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
