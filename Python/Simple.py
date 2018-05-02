a2fade1e086e43c8dd9888fff5a0c61796834507
cruiz1194 <vakamacarlos19_94@outlook.com>
2018-05-01 21:03:06 -0500

    Added some basic things to learn Python

--- Python/Simple.py
+++ Python/Simple.py
@@ -1,1 +1,23 @@
-print "Hola desde python"
+def suma(a,b):
+    result = a + b
+    return result
+
+def resta(a,b):
+    result = a - b
+    return result
+
+def multiplicacion(a,b):
+    result = a * b
+    print result
+
+
+
+print "Calculadora simple en Python"
+x = 5
+y = 6
+print suma(x,y)
+print resta(y,x)
+multiplicacion(5,6)
+
+for num in range(0,10):
+    print num
